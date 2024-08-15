8d87469fc33dde79ed2b9d87dfe6d4ff15d19257
Inavatta <ivannavatta@gmail.com>
2024-08-14 21:32:34 -0300

    fix(): Update app.config.ts

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -7,7 +7,7 @@ export default {
     dbPassword: process.env.DBPASSWORD,
     dbName: process.env.DBNAME,
     baseUrl: process.env.BASE_URL,
-    cloudCredencial: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    cloudCredencial: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS),
     cloudProjectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
     cloudBucketName: process.env.GOOGLE_CLOUD_BUCKET_NAME || ''
 }
