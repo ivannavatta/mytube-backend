76bc7df9c91d00aa15bf985bc188a43263d16ead
Inavatta <ivannavatta@gmail.com>
2026-05-14 20:37:49 -0300

    Restrict CORS to specific origin

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ limit: '200mb', extended: true }));
 
 app.use(express.static(__dirname + '/public'))
 
-app.use(cors());
+app.use(cors({ origin: "https://mytube-green.vercel.app"}));
 
 router(app)
 
